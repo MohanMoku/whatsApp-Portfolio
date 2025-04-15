c2190d745688e75e787dd1d1b7278fb3e81725c4
Mohan Kumar S <mohanmoku2004@gamil.com>
2025-04-15 21:26:15 +0530

    almost done

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,9 +1,9 @@
-import mokuPhoto from '../assets/mohan-kumar.png'
+import mokuPhoto from '../assets/moku-bg.png'
 
 export default function LandingPage() {
     return (
-        <div className='bg-gray-900 w-full h-full flex items-center justify-center rounded-3xl'>
-            <h1 className='text-7xl font-bold italic text-transparent' style={{ WebkitTextStroke: '1px white' }}>Mohan Kumar S</h1>
+        <div className='bg-gray-900 w-full h-full flex items-center justify-center rounded-3xl p-10'>
+            <h1 className='text-7xl font-bold italic text-transparent' style={{ WebkitTextStroke: '1px white' }}>Mohan Kumar</h1>
             <div className='flex justify-center h-150'>
                 <img src={mokuPhoto} alt="img"/>
             </div>
